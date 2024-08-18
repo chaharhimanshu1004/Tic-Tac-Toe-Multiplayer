8b151a4ec713667d942cb4ad942beaba3d842249
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2024-08-18 17:32:33 +0530

    updating socket connection

--- server/server.js
+++ server/server.js
@@ -1,37 +1,27 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+
+const { createServer } = require("http");
 const { Server } = require("socket.io");
-const cors = require('cors');
 
-let io;
 
-if (!server.io) {
-    io = new Server(server, {
-      cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-      }
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: "http://localhost:3000/",
+});
+
+io.on("connection", (socket) => {
+    console.log(`socket connected:${socket.id}`);
+    socket.on("disconnect", function () {
+        console.log(`socket disconnected: ${socket.id}`)
     });
-    server.io = io;
-    io.on('connection', (socket) => {
-        console.log(`Socket connected: ${socket.id}`);
-        socket.on('disconnect', () => {
-            console.log(`Socket disconnected: ${socket.id}`);
-            socket.broadcast.emit('user-disconnected', socket.id);
-        });
-    })
-} else {
-    io = server.io;
-    console.log('Socket already running');
-}
+})
 
 app.get('/', (req, res) => {
     res.send('<h1>server running!!</h1>');
 });
   
-server.listen(6001, () => {
+app.listen(6001, () => {
     console.log('listening on *:6001');
 });
 
